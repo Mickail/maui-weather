07fb9158cc200a7242fdaa7bcceff95075e19527
Hajrudin Mulalic <rudimulalic@gmail.com>
2025-03-07 11:10:40 +0100

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_weather;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace maui_weather;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
